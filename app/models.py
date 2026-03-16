dffb2c735cd0e4be1fd53de9652f1d04810f678f
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 12:08:07 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
+
 class Property(Base):
+
     __tablename__ = "properties"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -12,6 +14,7 @@ class Property(Base):
 
 
 class Vehicle(Base):
+
     __tablename__ = "vehicles"
 
     id = Column(Integer, primary_key=True, index=True)
