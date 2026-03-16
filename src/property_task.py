c25f171f3975596e4dad4b6b6bf9db62c83e82e6
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 15:40:34 -0500

    Delete src/property_task.py

--- src/property_task.py
+++
@@ -1,9 +0,0 @@
-from app.celery_worker import celery
-from agents.property_scraper import scrape_properties
-
-@celery.task
-def run_property_agent():
-
-    properties = scrape_properties()
-
-    return properties
