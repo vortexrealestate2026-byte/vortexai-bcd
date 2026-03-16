3534be0cdd8429b5dc03b22432ee9c29ed6fce8c
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 16:02:54 -0500

    Update agent_scheduler.py

--- src/tasks/agent_scheduler.py
+++ src/tasks/agent_scheduler.py
@@ -1,5 +1,6 @@
 import asyncio
 from src.tasks.property_task import run_property_agent
+from src.tasks.vehicle_agent import run_vehicle_agent
 
 async def launch_all_agents():
 
@@ -7,6 +8,7 @@ async def launch_all_agents():
 
     agents = [
         asyncio.create_task(run_property_agent()),
+        asyncio.create_task(run_vehicle_agent()),
     ]
 
     await asyncio.gather(*agents)
